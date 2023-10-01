d0804b7465d67ff033e910e4c5dd5b9a9dad91fd
Luna T <luna_trn@tutanota.com>
2023-10-01 17:48:07 +1100

    Initial Commit

---
+++ src/main/java/xyz/lunatrn/Main.java
@@ -0,0 +1,14 @@
+package xyz.lunatrn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}

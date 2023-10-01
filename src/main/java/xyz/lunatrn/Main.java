02e0746388a7461f09bbea0da3cf00206a794ddb
Luna T <luna_trn@tutanota.com>
2023-10-01 17:57:12 +1100

    Added Enable / Disable Messages
    Author: Luna T

--- src/main/java/xyz/lunatrn/Main.java
+++ src/main/java/xyz/lunatrn/Main.java
@@ -2,13 +2,19 @@ package xyz.lunatrn;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public final class Main extends JavaPlugin {
 
+    Logger logger = this.getLogger();
     @Override
     public void onEnable() {
+        this.logger.log(Level.INFO,"JJK-Domains Enabled");
     }
 
     @Override
     public void onDisable() {
+        this.logger.log(Level.INFO,"JJK-Domains Disabled");
     }
 }
